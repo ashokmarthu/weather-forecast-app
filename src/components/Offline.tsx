b968c88f59cfd00cae4f1fcde8db7bf8e80c06bd
Ashok Babu Marthu <ashokmarthu999@gmail.com>
2025-02-17 22:03:27 +0530

    fix build issue

--- src/components/Offline.tsx
+++ src/components/Offline.tsx
@@ -1,10 +1,11 @@
 import { IoCloudOffline } from "react-icons/io5";
 
 const Offline = () => {
+  const OFFLINE_STATUS = "Hey, Your'e Offline";
   return (
     <div className="flex flex-col items-center gap-2 justify-center py-8">
       <IoCloudOffline className="h-20 w-20 animate-pulse" />
-      <h2 className="text-2xl">Hey, Your'e Offline</h2>
+      <h2 className="text-2xl">{OFFLINE_STATUS} </h2>
       <small className="text-xl">
         Try, Checking the network cables, modem, and router Reconnecting to
         Wi-Fi / Internet Connection
