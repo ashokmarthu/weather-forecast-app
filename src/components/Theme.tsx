2baf1c64b7b39de64a96b8288320e66d69190a62
Ashok Babu Marthu <ashokmarthu999@gmail.com>
2025-02-26 20:48:32 +0530

    fix to stop toggle when we click on text

--- src/components/Theme.tsx
+++ src/components/Theme.tsx
@@ -4,36 +4,30 @@ import { DARK_MODE, LIGHT_MODE } from "./utils/constants";
 const Theme = () => {
   const { theme, setTheme } = useTheme();
   const isChecked = theme === DARK_MODE;
+
+  const handleToggle = (e: React.MouseEvent<HTMLButtonElement>) => {
+    setTheme((prev) => (prev === LIGHT_MODE ? DARK_MODE : LIGHT_MODE));
+  };
+
   return (
-    <>
-      <label className="relative inline-flex cursor-pointer select-none items-center">
-        <input
-          type="checkbox"
-          checked={isChecked}
-          onChange={() =>
-            setTheme((prev) => (prev === LIGHT_MODE ? DARK_MODE : LIGHT_MODE))
-          }
-          className="sr-only"
-        />
-        <span className="label flex items-center text-sm font-medium">
-          Light
-        </span>
+    <div className="relative inline-flex cursor-pointer select-none items-center">
+      <span className="label flex items-center text-sm font-medium">Light</span>
+      <button
+        className={`mx-4 flex h-8 w-[60px] items-center rounded-full p-1 duration-200 ${
+          isChecked ? "bg-[#212b36]" : "bg-[#CCCCCE]"
+        }`}
+        role="switch"
+        onClick={handleToggle}
+      >
         <span
-          className={`mx-4 flex h-8 w-[60px] items-center rounded-full p-1 duration-200 ${
-            isChecked ? "bg-[#212b36]" : "bg-[#CCCCCE]"
+          className={`h-6 w-6 rounded-full bg-white duration-200 ${
+            isChecked ? "translate-x-[28px]" : ""
           }`}
-        >
-          <span
-            className={`h-6 w-6 rounded-full bg-white duration-200 ${
-              isChecked ? "translate-x-[28px]" : ""
-            }`}
-          ></span>
-        </span>
-        <span className="label flex items-center text-sm font-medium">
-          Dark
-        </span>
-      </label>
-    </>
+        ></span>
+      </button>
+      <span className="label flex items-center text-sm font-medium">Dark</span>
+    </div>
   );
 };
+
 export default Theme;
